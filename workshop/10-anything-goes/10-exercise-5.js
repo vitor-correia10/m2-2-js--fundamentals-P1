472feee7145d7b000f72bcfc5aff2506fa217ce6
Scott Morin <scottanthonymorin@gmail.com>
2020-02-05 09:53:39 -0500

    fix typo

--- workshop/10-anything-goes/10-exercise-5.js
+++ workshop/10-anything-goes/10-exercise-5.js
@@ -6,7 +6,7 @@
 // Note : An Armstrong number of three digits is an integer 
 // such that the sum of the cubes of its digits is equal 
 // to the number itself. For example,
-// 371 is an Armstrong number since 3*3 + 7*3 + 1*3 = 371.
+// 371 is an Armstrong number since 3^3 + 7^3 + 1^3 = 371.
 
 let armstrongNumbers = [];
 
